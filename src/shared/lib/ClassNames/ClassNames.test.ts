03b81c0b38169e891a33d8ec742469635626a23a
bahniiMDev <bahniimike@gmail.com>
2024-07-13 14:43:54 +0300

    passed to 2.20 part install jest and setup + add tests on ClassNames func int 5

---
+++ src/shared/lib/ClassNames/ClassNames.test.ts
@@ -0,0 +1,19 @@
+import { ClassNames } from './ClassNames';
+
+describe('ClassNames', () => {
+  test('with one params', () => {
+    expect(ClassNames('class')).toBe('class');
+  });
+  test('with one params and two obj', () => {
+    expect(ClassNames('class', { active: true })).toBe('class active');
+  });
+  test('with one params and two obj one false', () => {
+    expect(ClassNames('class', { active: true, hover: false })).toBe('class active');
+  });
+  test('with one params and two obj one undefined', () => {
+    expect(ClassNames('class', { active: true, hover: undefined })).toBe('class active');
+  });
+  test('with one params and two obj and arr', () => {
+    expect(ClassNames('class', { active: true }, ['add_class'])).toBe('class active add_class');
+  });
+});
--- src/shared/lib/ClassNames/ClassNames.ts
+++ src/shared/lib/ClassNames/ClassNames.ts
@@ -1,11 +1,11 @@
-type Modsa = Record<string, boolean>;
+type Mods = Record<string, boolean>;
 
-export function ClassNames(cls: string, modify?: Modsa, more: string[] = []): string {
+export function ClassNames(cls: string, modify: Mods = {}, more: string[] = []): string {
   return [
     cls,
-    ...more.filter(Boolean),
     ...Object.entries(modify || {})
       .filter(arr => arr[1])
-      .map(arr => arr[0])
+      .map(arr => arr[0]),
+    ...more.filter(Boolean)
   ].join(' ');
 }
